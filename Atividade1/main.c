f3b62f40cf9af237d0ebcdcb4bf8de1edc71c525
Alyson Matheus <alyson1907@gmail.com>
2020-04-15 11:57:42 -0300

    ^main.c: receiveing matrixSize in argv, +README.md

--- Atividade1/main.c
+++ Atividade1/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  for(int i = 1; i <= 10; i++) {
-    printf("i %d\n", i);
+int main(int argc, char **argv) {
+  // Reding the matrix size
+  int matrixSize = atoi(argv[1]);
+  for(int i = 1; i <= matrixSize; i++) {
+    for (int j = 1; j <= matrixSize; j++) {
+      printf("i: %d, j: %d\n", i, j);
+    }
   }
 }
