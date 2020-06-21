e1bdbda8f3c63d07d25b977a81abdc239c001fe4
Alyson Matheus <alyson.nascimento@livenbr.com>
2020-06-21 15:43:55 -0300

    +clean_cache() code @Atividade3/main.c

--- Atividade3/main.c
+++ Atividade3/main.c
@@ -4,6 +4,17 @@
 
 // Array Length
 #define size 20
+/* Code to remove data from the processor caches. */ 
+#define KB (1024)
+#define MB (1024 * KB)
+#define GB (1024 * MB)
+#define LARGEST_CACHE_SZ (6144 * KB)
+
+static unsigned char dummy_buffer[LARGEST_CACHE_SZ];
+void clean_cache() {
+  unsigned long long i;
+  for (i = 0; i < LARGEST_CACHE_SZ; i++) dummy_buffer[i] += 1;
+}
 
 // *a is the original array
 // *b is the resulting array
