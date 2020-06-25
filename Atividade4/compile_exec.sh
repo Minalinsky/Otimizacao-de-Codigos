1fcf2bd56886b8e4feef19024880072ca7f6565a
Alyson Matheus <alyson1907@gmail.com>
2020-06-25 19:41:01 -0300

    +flags from paper @compile_exec.sh

--- Atividade4/compile_exec.sh
+++ Atividade4/compile_exec.sh
@@ -24,8 +24,30 @@ gcc -O3 -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
 sudo perf stat -o ./results/O3_perf.txt ./fasta.gcc-2.exe 9000000 > temp
 
 rm temp
-echo "Done."
 
 
 # Now compiling with the flags that appear more than +80% for the `fasta` program in 
 # paper `Finding  Best  Compiler Options for Critical Software Using Parallel Algorithms`
+
+# -fdce flag
+gcc -fdce -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
+sudo perf stat -o ./results/fdce_perf.txt ./fasta.gcc-2.exe 9000000 > temp
+
+# -finline flag
+gcc -finline -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
+sudo perf stat -o ./results/finline_perf.txt ./fasta.gcc-2.exe 9000000 > temp
+
+# -fpeephole2 flag
+gcc -fpeephole2 -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
+sudo perf stat -o ./results/fpeephole2_perf.txt ./fasta.gcc-2.exe 9000000 > temp
+
+# -ftree-ch flag
+gcc -ftree-ch -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
+sudo perf stat -o ./results/ftree-ch_perf.txt ./fasta.gcc-2.exe 9000000 > temp
+
+# -fif-conversion flag
+gcc -fif-conversion -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
+sudo perf stat -o ./results/fif-conversion_perf.txt ./fasta.gcc-2.exe 9000000 > temp
+
+
+echo "Done."
