8e05b1b21446f4d37fb62f3643cabb48bbd54936
Alyson Matheus <alyson1907@gmail.com>
2020-06-25 19:36:06 -0300

    ^ fasta input to 9000000, +user feedback in compile_exec.sh

--- Atividade4/compile_exec.sh
+++ Atividade4/compile_exec.sh
@@ -5,24 +5,26 @@
 #   - O3 optimation flag
 
 # After each compilation, it executes `perf` so we can see the statistics for each compilation
-mkdir results/
+echo "Running scripts..."
+mkdir -p results/
 # No optimization
 gcc -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
-sudo perf stat -o ./results/no_opt.txt ./fasta.gcc-2.exe 5000000 > temp # temp is used to store program output so we dont need to wait for the prints in terminal to finish
+sudo perf stat -o ./results/no_opt.txt ./fasta.gcc-2.exe 9000000 > temp # temp is used to store program output so we dont need to wait for the prints in terminal to finish
 
 # O1 optimization
 gcc -O1 -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
-sudo perf stat -o ./results/O1_perf.txt ./fasta.gcc-2.exe 5000000 > temp
+sudo perf stat -o ./results/O1_perf.txt ./fasta.gcc-2.exe 9000000 > temp
 
 # O2 optimization
 gcc -O2 -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
-sudo perf stat -o ./results/O2_perf.txt ./fasta.gcc-2.exe 5000000 > temp
+sudo perf stat -o ./results/O2_perf.txt ./fasta.gcc-2.exe 9000000 > temp
 
 # O3 optimization
 gcc -O3 -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
-sudo perf stat -o ./results/O3_perf.txt ./fasta.gcc-2.exe 5000000 > temp
+sudo perf stat -o ./results/O3_perf.txt ./fasta.gcc-2.exe 9000000 > temp
 
 rm temp
+echo "Done."
 
 
 # Now compiling with the flags that appear more than +80% for the `fasta` program in 
