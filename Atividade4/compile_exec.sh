3aa56617de6d04f01d122785aa0ef1ee96c12235
Alyson Matheus <alyson1907@gmail.com>
2020-06-25 18:19:13 -0300

    +Compile with paper flags section @compile_exec.sh

--- Atividade4/compile_exec.sh
+++ Atividade4/compile_exec.sh
@@ -5,7 +5,6 @@
 #   - O3 optimation flag
 
 # After each compilation, it executes `perf` so we can see the statistics for each compilation
-
 mkdir results/
 # No optimization
 gcc -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
@@ -22,3 +21,7 @@ sudo perf stat ./fasta.gcc-2.exe 5000000 > ./results/O2_perf.txt
 # O3 optimization
 gcc -O3 -march=core2 fasta.gcc-2.c -o fasta.gcc-2.exe
 sudo perf stat ./fasta.gcc-2.exe 5000000 > ./results/O3_perf.txt
+
+
+# Now compiling with the flags that appear more than +80% for the `fasta` program in 
+# paper `Finding  Best  Compiler Options for Critical Software Using Parallel Algorithms`
